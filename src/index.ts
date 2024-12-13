e64ebb85bfd299dffacd41d240d47b79ca15f1b1
BreicheSinal <sinalbreiche@gmail.com>
2024-12-13 23:00:06 +0200

    Called connect to db function in index to establish connection

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { init } from "./config/init";
 import express, { Express } from "express";
+import { connectToDatabase } from "./db/connection";
 
 const app: Express = express();
 
@@ -7,4 +8,5 @@ init(app);
 
 app.listen(process.env.SERVER_PORT, () => {
   console.log(`Server running at http://localhost:${process.env.SERVER_PORT}`);
+  connectToDatabase();
 });
