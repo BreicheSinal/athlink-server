bc13fe62e437b3f615813716a146bda4168be4b6
BreicheSinal <sinalbreiche@gmail.com>
2024-12-13 22:52:23 +0200

    Set up connection with db

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,3 +1,4 @@
+import mysql from "mysql2/promise";
 import dotenv from "dotenv";
 
 // loading variables from .env
@@ -15,3 +16,30 @@ interface Env {
 // destructuring variables
 const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT }: Env =
   process.env as unknown as Env;
+
+// set up db connection
+(async () => {
+  try {
+    const dbPort = Number(DB_PORT);
+
+    if (isNaN(dbPort) || dbPort <= 0) {
+      throw new Error(`Invalid DB_PORT`);
+    }
+
+    const db = await mysql.createConnection({
+      host: DB_HOST,
+      user: DB_USER,
+      password: DB_PASSWORD,
+      database: DB_NAME,
+      port: dbPort,
+    });
+
+    console.log(`Connected to database successfully!`);
+  } catch (error: unknown) {
+    const errorMessage =
+      error instanceof Error ? error.message : "Unknown error occurred";
+    console.error(`Error: ${errorMessage}`);
+
+    process.exit(1);
+  }
+})();
