bb364930a43cc3d64864d0d52e62a216f7ffefa9
BreicheSinal <sinalbreiche@gmail.com>
2024-12-13 18:59:54 +0200

    Added cors package to handle cross-origin requests

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express } from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 const app: Express = express();
+
 dotenv.config();
+
+app.use(cors());
 
 app.listen(process.env.SERVER_PORT, () => {
   console.log(`Server running at http://localhost:${process.env.SERVER_PORT}`);
