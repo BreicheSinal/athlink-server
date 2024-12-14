8e7323eca69effc03c40fb76af3db2c3215f0a58
BreicheSinal <sinalbreiche@gmail.com>
2024-12-14 19:38:45 +0200

    Updated connection to db file

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,6 +1,7 @@
 import { DataSource } from "typeorm";
 import dotenv from "dotenv";
 import "reflect-metadata";
+import { join } from "path";
 
 // loading variables from .env
 dotenv.config();
@@ -33,7 +34,7 @@ const AppDataSource = new DataSource({
   database: DB_NAME,
   synchronize: false,
   logging: true,
-  //entities:[],
+  entities: [join(__dirname, "src/entities/**/*.{ts}")],
   migrations: [],
   subscribers: [],
 });
