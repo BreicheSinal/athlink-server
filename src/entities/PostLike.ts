e791cb20a6663e882d4310b3e69aa142184e0c18
BreicheSinal <sinalbreiche@gmail.com>
2024-12-14 17:29:58 +0200

    Added post like entity

---
+++ src/entities/PostLike.ts
@@ -0,0 +1,33 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  ManyToOne,
+  JoinColumn,
+  CreateDateColumn,
+  Index,
+} from "typeorm";
+
+import { Post } from "./Post";
+import { User } from "./User";
+
+@Entity("post_like")
+@Index(["post"])
+@Index(["user"])
+export class PostLike {
+  @PrimaryGeneratedColumn("increment", {
+    type: "bigint",
+    unsigned: true,
+  })
+  id!: number;
+
+  @ManyToOne(() => Post, (post) => post.id, { onDelete: "CASCADE" })
+  @JoinColumn({ name: "post_id" })
+  post!: Post;
+
+  @ManyToOne(() => User, (user) => user.id, { onDelete: "CASCADE" })
+  @JoinColumn({ name: "user_id" })
+  user!: User;
+
+  @CreateDateColumn({ type: "datetime" })
+  createdAt!: Date;
+}
