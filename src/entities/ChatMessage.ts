cd18af6433c5a9133b84dcb6a1e6376f9e7ae539
BreicheSinal <sinalbreiche@gmail.com>
2024-12-14 17:36:28 +0200

    Added chat message entity

--- src/entities/ChatMessage.ts
+++ src/entities/ChatMessage.ts
@@ -1,30 +1,36 @@
 import {
   Entity,
   PrimaryGeneratedColumn,
+  Column,
   ManyToOne,
   JoinColumn,
   CreateDateColumn,
   Index,
 } from "typeorm";
 
+import { Chat } from "./Chat";
 import { User } from "./User";
 
-@Entity("chats")
-@Index(["user_id_1", "user_id_2"])
-export class Chat {
+@Entity("chat_message")
+@Index("chat_id_index", ["chat_id"])
+@Index("sender_id_index", ["sender_id"])
+export class ChatMessage {
   @PrimaryGeneratedColumn("increment", {
     type: "bigint",
     unsigned: true,
   })
   id!: number;
 
-  @ManyToOne(() => User, (user) => user.id, { onDelete: "CASCADE" })
-  @JoinColumn({ name: "user_id_1" })
-  user1!: User;
+  @ManyToOne(() => Chat, (chat) => chat.id, { onDelete: "CASCADE" })
+  @JoinColumn({ name: "chat_id" })
+  chat!: Chat;
 
   @ManyToOne(() => User, (user) => user.id, { onDelete: "CASCADE" })
-  @JoinColumn({ name: "user_id_2" })
-  user2!: User;
+  @JoinColumn({ name: "sender_id" })
+  sender!: User;
+
+  @Column("text")
+  message!: string;
 
   @CreateDateColumn({ type: "datetime" })
   createdAt!: Date;
