a8790e182fda3e7474245340f2bbb4f2d4a3d019
BreicheSinal <sinalbreiche@gmail.com>
2024-12-14 02:20:18 +0200

    Added user entity

---
+++ src/entities/User.ts
@@ -0,0 +1,61 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
+} from "typeorm";
+import { Role } from "./Role";
+
+@Entity("user")
+export class User {
+  @PrimaryGeneratedColumn({
+    type: "bigint",
+    unsigned: true,
+  })
+  id!: number;
+
+  @Column({
+    type: "varchar",
+    length: 255,
+    unique: true,
+  })
+  name!: string;
+
+  @Column({
+    type: "varchar",
+    length: 255,
+  })
+  password!: string;
+
+  @Column({
+    type: "varchar",
+    length: 255,
+    unique: true,
+  })
+  email!: string;
+
+  @Column({
+    type: "tinyint",
+    unsigned: true,
+  })
+  role_id!: number;
+
+  @ManyToOne(() => Role, (role) => role.id, { eager: true })
+  @JoinColumn({ name: "role_id" })
+  role!: Role;
+
+  @Column({
+    type: "text",
+    nullable: true,
+  })
+  bio?: string;
+
+  @CreateDateColumn({ type: "datetime" })
+  created_at!: Date;
+
+  @UpdateDateColumn({ type: "datetime" })
+  updated_at!: Date;
+}
