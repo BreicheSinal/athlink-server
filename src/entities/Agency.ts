c4bf854a91847bbd850c663bcaa9421d8206ce07
BreicheSinal <sinalbreiche@gmail.com>
2024-12-14 03:35:57 +0200

    Updated agency entity

--- src/entities/Agency.ts
+++ src/entities/Agency.ts
@@ -1,44 +1,43 @@
 import {
-    Entity,
-    PrimaryGeneratedColumn,
-    Column,
-    OneToOne,
-    JoinColumn,
-    CreateDateColumn,
-    UpdateDateColumn,
-  } from "typeorm";
-  
-  import { User } from "./User";
-  
-  @Entity("agency")
-  export class Agency {
-    @PrimaryGeneratedColumn("increment", {
-      type: "bigint",
-      unsigned: true,
-    })
-    id!: number;
-  
-    @OneToOne(() => User, (user) => user.id)
-    @JoinColumn({ name: "user_id" })
-    user!: User;
-  
-    @Column({
-      type: "varchar",
-      length: 255,
-      nullable: true,
-    })
-    location?: string;
-  
-    @Column({
-      type: "int",
-      nullable: true,
-    })
-    foundedYear?: number;
-  
-    @CreateDateColumn({ type: "datetime" })
-    createdAt!: Date;
-  
-    @UpdateDateColumn({ type: "datetime" })
-    updatedAt!: Date;
-  }
-  
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  JoinColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+} from "typeorm";
+
+import { User } from "./User";
+
+@Entity("agency")
+export class Agency {
+  @PrimaryGeneratedColumn("increment", {
+    type: "bigint",
+    unsigned: true,
+  })
+  id!: number;
+
+  @ManyToOne(() => User, (user) => user.id)
+  @JoinColumn({ name: "user_id" })
+  user!: User;
+
+  @Column({
+    type: "varchar",
+    length: 255,
+    nullable: true,
+  })
+  location?: string;
+
+  @Column({
+    type: "int",
+    nullable: true,
+  })
+  foundedYear?: number;
+
+  @CreateDateColumn({ type: "datetime" })
+  createdAt!: Date;
+
+  @UpdateDateColumn({ type: "datetime" })
+  updatedAt!: Date;
+}
