52bed1b406f4ea8d9f86878f2211ad94fad899e3
BreicheSinal <sinalbreiche@gmail.com>
2024-12-14 17:44:14 +0200

    Added try out entity

---
+++ src/entities/TryOut.ts
@@ -0,0 +1,51 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  JoinColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  Index,
+} from "typeorm";
+
+import { Club } from "./Club";
+
+@Entity("try_out")
+@Index("idx_club_id", ["clubId"])
+@Index("idx_date", ["date"])
+@Index("idx_name", ["name"])
+export class TryOut {
+  @PrimaryGeneratedColumn("increment", {
+    type: "bigint",
+    unsigned: true,
+  })
+  id!: number;
+
+  @ManyToOne(() => Club, (club) => club.id)
+  @JoinColumn({ name: "club_id" })
+  club!: Club;
+
+  @Column({
+    type: "varchar",
+    length: 255,
+  })
+  name!: string;
+
+  @Column({
+    type: "datetime",
+  })
+  date!: Date;
+
+  @Column({
+    type: "text",
+    nullable: true,
+  })
+  description?: string;
+
+  @CreateDateColumn({ type: "datetime" })
+  createdAt!: Date;
+
+  @UpdateDateColumn({ type: "datetime" })
+  updatedAt!: Date;
+}
