41e265a33fa2db3655c717f83ca4e0e605fd8088
BreicheSinal <sinalbreiche@gmail.com>
2024-12-14 02:58:51 +0200

    Added athlete entity

---
+++ src/entities/Athlete.ts
@@ -0,0 +1,64 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  JoinColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from "typeorm";
+
+import { User } from "./User";
+import { Club } from "./Club";
+
+@Entity("athlete")
+export class Athlete {
+  @PrimaryGeneratedColumn("increment", {
+    type: "bigint",
+    unsigned: true,
+  })
+  id!: number;
+
+  @ManyToOne(() => User, (user) => user.id)
+  @JoinColumn({ name: "user_id" })
+  user!: User;
+
+  @ManyToOne(() => Club, (club) => club.id)
+  @JoinColumn({ name: "club_id" })
+  club!: Club;
+
+  @Column({
+    type: "varchar",
+    length: 255,
+    nullable: true,
+  })
+  position?: string;
+
+  @Column({
+    type: "int",
+    nullable: true,
+  })
+  age?: number;
+
+  @Column({
+    type: "decimal",
+    precision: 5,
+    scale: 2,
+    nullable: true,
+  })
+  height?: number;
+
+  @Column({
+    type: "decimal",
+    precision: 5,
+    scale: 2,
+    nullable: true,
+  })
+  weight?: number;
+
+  @CreateDateColumn({ type: "datetime" })
+  createdAt!: Date;
+
+  @UpdateDateColumn({ type: "datetime" })
+  updatedAt!: Date;
+}
