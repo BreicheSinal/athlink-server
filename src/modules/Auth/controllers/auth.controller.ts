c3e9be0586b38e04ebed766d04b3751c245ea09b
BreicheSinal <sinalbreiche@gmail.com>
2024-12-15 23:33:27 +0200

    Valited input in register

--- src/modules/Auth/controllers/auth.controller.ts
+++ src/modules/Auth/controllers/auth.controller.ts
@@ -2,9 +2,23 @@ import { AppDataSource } from "../../../db/connection";
 import { User } from "../../../entities/User";
 import { Role } from "../../../entities/Role";
 import { UserRole } from "../../../entities/UserRole";
+import { throwError, throwNotFound } from "../../../utils/error";
 
 const userRepository = AppDataSource.getRepository(User);
 const roleRepository = AppDataSource.getRepository(Role);
 const userRoleRepository = AppDataSource.getRepository(UserRole);
 
-export const Regist = async (req: any, res: any) => {};
+export const Regist = async (req: any, res: any) => {
+  try {
+    const { name, email, password, bio, roles } = req.body;
+
+    // validating input
+    if (!name || !email || !password || !roles) {
+      return throwError({
+        message: "Missing required fields",
+        res,
+        status: 400,
+      });
+    }
+  } catch {}
+};
