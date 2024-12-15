c9d4913601d7b27f39b361453c266c2a846706a7
BreicheSinal <sinalbreiche@gmail.com>
2024-12-16 01:40:54 +0200

    Tested route

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ import { init } from "./config/init";
 import express, { Express } from "express";
 import { connectToDatabase } from "./db/connection";
 import "reflect-metadata";
+import auth from "./modules/Auth/auth.routes";
 
 const app: Express = express();
 
 init(app);
+app.use("/", auth);
 
 app.listen(process.env.SERVER_PORT, async () => {
   console.log(`Server running at http://localhost:${process.env.SERVER_PORT}`);
--- src/modules/Auth/auth.routes.ts
+++ src/modules/Auth/auth.routes.ts
@@ -2,4 +2,8 @@ import { Router } from "express";
 
 const router = Router();
 
+router.get("/test", (req, res) => {
+  res.status(200).send("Test route is working!");
+});
+
 export default router;
