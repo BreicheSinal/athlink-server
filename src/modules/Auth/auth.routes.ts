73e24806b672afe663b70e110ac5d1a8093fb31c
BreicheSinal <sinalbreiche@gmail.com>
2024-12-19 12:58:42 +0200

    Added registration limiter middleware

--- src/modules/Auth/auth.routes.ts
+++ src/modules/Auth/auth.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { register, login } from "./auth.controller";
+import { registrationLimiter } from "../middlewares/rate.limiter";
 
 const router = Router();
 
-router.post("/register", register);
+router.post("/register", registrationLimiter, register);
 router.post("/login", login);
 
 export default router;
