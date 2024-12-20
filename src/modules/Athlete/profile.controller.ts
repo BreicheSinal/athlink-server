9d26cabd598b81db60f0c3a2b07b39264679a054
BreicheSinal <sinalbreiche@gmail.com>
2024-12-20 17:56:50 +0200

    Created an experience certification object

--- src/modules/Athlete/profile.controller.ts
+++ src/modules/Athlete/profile.controller.ts
@@ -365,12 +365,20 @@ export const addExperience = async (req: Request, res: Response) => {
     }
 
     // validating description
-    if (!description || typeof description !== "string")
+    if (description && typeof description !== "string")
       return throwError({
         message: "Description must be non empty text",
         res,
         status: 400,
       });
+
+    //creating experienceCertification object
+    const experienceCertification = new ExperienceCertification();
+    experienceCertification.user = user;
+    experienceCertification.name = name;
+    experienceCertification.type = type;
+    experienceCertification.date = date;
+    experienceCertification.description = description;
   } catch (error: unknown) {
     const errorMessage =
       error instanceof Error ? error.message : "Unknown error occurred";
