26d04b7ab80e817238a8cc1a1955a85447fd6cd0
BreicheSinal <sinalbreiche@gmail.com>
2024-12-20 16:53:15 +0200

    Added a route for adding trophy

--- src/modules/Athlete/profile.routes.ts
+++ src/modules/Athlete/profile.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { editProfile, editBio } from "./profile.controller";
+import { editProfile, editBio, addTrophy } from "./profile.controller";
 
 const router = Router();
 
 router.put("/editProfile/:id", editProfile as any);
 router.put("/editBio/:id", editBio as any);
+router.post("/addTrophy/:entity_id", addTrophy as any);
 
 export default router;
