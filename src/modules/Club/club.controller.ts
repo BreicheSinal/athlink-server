501459d6d1fb94f135c72be7db66c2be4d544933
BreicheSinal <sinalbreiche@gmail.com>
2024-12-22 05:23:50 +0200

    Validated club id --getStaff --club

--- src/modules/Club/club.controller.ts
+++ src/modules/Club/club.controller.ts
@@ -155,3 +155,34 @@ export const editBio = async (req: Request, res: Response) => {
     });
   }
 };
+
+export const getStaff = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params; //club id
+
+    // validating ID
+    if (!id)
+      return throwError({
+        message: "ID required",
+        res,
+        status: 400,
+      });
+
+    const parsedId = parseInt(id);
+    if (isNaN(parsedId) || parsedId <= 0) {
+      return throwError({
+        message: "ID must be a positive integer",
+        res,
+        status: 400,
+      });
+    }
+  } catch (error: unknown) {
+    const errorMessage =
+      error instanceof Error ? error.message : "Unknown error occurred";
+    console.error(`Error: ${errorMessage}`);
+    return throwError({
+      message: errorMessage,
+      res,
+    });
+  }
+};
