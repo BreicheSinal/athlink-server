0a02ee721dee1b4d63940eeb48957a82d6e174b5
BreicheSinal <sinalbreiche@gmail.com>
2024-12-22 04:03:35 +0200

    Added verification trophy method

--- src/modules/Trophy/trophy.controller.ts
+++ src/modules/Trophy/trophy.controller.ts
@@ -7,6 +7,7 @@ const trophyService = new TrophyService();
 export const requestTrophy = async (req: Request, res: Response) => {
   try {
     const { name, description } = req.body;
+
     const trophyId = await trophyService.requestTrophy(name, description);
 
     res.status(201).json({ trophyId });
@@ -16,6 +17,24 @@ export const requestTrophy = async (req: Request, res: Response) => {
     throwError({
       message: "Failed to request trophy",
       res,
+    });
+  }
+};
+
+export const verifyTrophy = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const { approved } = req.body;
+
+    await trophyService.verifyTrophy(Number(id), approved);
+
+    res.json({ success: true });
+  } catch (error) {
+    console.error(error);
+
+    throwError({ 
+        message: "Failed to verify trophy", 
+        res 
     });
   }
 };
