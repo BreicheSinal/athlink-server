117bde812e808ebce7694191f3658fff9877c694
BreicheSinal <sinalbreiche@gmail.com>
2024-12-22 03:53:38 +0200

    Added a function to verify a trophy

--- src/modules/Trophy/trophy.service.ts
+++ src/modules/Trophy/trophy.service.ts
@@ -5,8 +5,8 @@ export class TrophyService {
   // function to request a trophy --- sending a transaction to small contract
   async requestTrophy(name: string, description: string): Promise<number> {
     try {
-      const tx = await contract.requestTrophy(name, description);
-      const receipt = await tx.wait();
+      const transaction = await contract.requestTrophy(name, description);
+      const receipt = await transaction.wait();
 
       const event = receipt.logs
         .map((log: any) => contract.interface.parseLog(log))
@@ -17,6 +17,17 @@ export class TrophyService {
       return Number(event.args[0]);
     } catch (error) {
       console.error("Error requesting trophy:", error);
+      throw error;
+    }
+  }
+
+  // function to verify a trophy
+  async verifyTrophy(trophyId: number, approved: boolean): Promise<void> {
+    try {
+      const transaction = await contract.verifyTrophy(trophyId, approved);
+      await transaction.wait();
+    } catch (error) {
+      console.error("Error verifying trophy:", error);
       throw error;
     }
   }
