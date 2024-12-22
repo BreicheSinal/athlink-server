3b3ae3063afc710f7c4bfdc94a5cc3e576385ba0
BreicheSinal <sinalbreiche@gmail.com>
2024-12-22 14:06:57 +0200

    Added route --get staff method

--- src/modules/Club/club.routes.ts
+++ src/modules/Club/club.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { editProfile, editBio } from "./club.controller";
+import { editProfile, editBio, getStaff } from "./club.controller";
 
 const router = Router();
 
 router.put("/editProfile/:id", editProfile as any);
 router.put("/editBio/:id", editBio as any);
+
+router.get("/getStaff/:id", getStaff as any);
 
 export default router;
