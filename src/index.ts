19959b48b5e56272b953ccfe712836d715f6028f
BreicheSinal <sinalbreiche@gmail.com>
2024-12-22 04:14:12 +0200

    Added routes for trophy controller

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { connectToDatabase } from "./db/connection";
 import "reflect-metadata";
 import auth from "./modules/Auth/auth.routes";
 import athlete from "./modules/Athlete/profile.routes";
+import trophy from "./modules/Trophy/trophy.routes";
 
 const app: Express = express();
 
@@ -13,6 +14,7 @@ app.use(express.json());
 
 app.use("/", auth);
 app.use("/", athlete);
+app.use("/", trophy);
 
 app.listen(process.env.SERVER_PORT, async () => {
   console.log(`Server running at http://localhost:${process.env.SERVER_PORT}`);
