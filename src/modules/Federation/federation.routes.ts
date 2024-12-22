50a354f92da112022dcc1c53186b8ce8ecb351c3
BreicheSinal <sinalbreiche@gmail.com>
2024-12-22 16:07:36 +0200

    Added route for get clubs method

--- src/modules/Federation/federation.routes.ts
+++ src/modules/Federation/federation.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { editProfile, editBio } from "./federation.controller";
+import { editProfile, editBio, getClubs } from "./federation.controller";
 
 const router = Router();
 
 router.put("/editProfile/:id", editProfile as any);
 router.put("/editBio/:id", editBio as any);
+
+router.get("/getClubs/:id", getClubs as any);
 
 export default router;
