baf59f55bb2a05c21da9c3de79953a0b87df63c7
BreicheSinal <sinalbreiche@gmail.com>
2024-12-25 01:55:36 +0200

    Added get club method --club-controller

--- src/modules/Auth/auth.routes.ts
+++ src/modules/Auth/auth.routes.ts
@@ -10,6 +10,7 @@ const router = Router();
 router.post("/register", registrationLimiter, register as any);
 //router.post("/register", register as any);
 
-router.post("/login", loginLimiter, login as any);
+//router.post("/login", loginLimiter, login as any);
+router.post("/login", login as any);
 
 export default router;
--- src/modules/Club/club.controller.ts
+++ src/modules/Club/club.controller.ts
@@ -206,3 +206,53 @@ export const getStaff = async (req: Request, res: Response) => {
     });
   }
 };
+
+export const getClub = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params; // club id
+
+    // validating ID
+    if (!id)
+      return throwError({
+        message: "ID required",
+        res,
+        status: 400,
+      });
+
+    const parsedId = parseInt(id);
+    if (isNaN(parsedId) || parsedId <= 0) {
+      return throwError({
+        message: "ID must be a positive integer",
+        res,
+        status: 400,
+      });
+    }
+
+    // fetching athlete by ID
+    const club = await clubRepository.find({
+      where: { id: parsedId },
+      relations: ["user", "federation"],
+    });
+
+    if (club.length === 0) {
+      return throwNotFound({
+        entity: `Club with id ${id}`,
+        check: true,
+        res,
+      });
+    }
+
+    return res.status(200).json({
+      message: "Club fetched successfully",
+      Club: club,
+    });
+  } catch (error: unknown) {
+    const errorMessage =
+      error instanceof Error ? error.message : "Unknown error occurred";
+    console.error(`Error: ${errorMessage}`);
+    return throwError({
+      message: errorMessage,
+      res,
+    });
+  }
+};
