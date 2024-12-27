08fc081406c08ae274889bbe569cfc52104db617
BreicheSinal <sinalbreiche@gmail.com>
2024-12-27 04:17:51 +0200

    Added route --edit-bio-coach

--- src/modules/Coach/coach.routes.ts
+++ src/modules/Coach/coach.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { editProfile, getCoach } from "./coach.controller";
+import { editProfile, getCoach, editBio } from "./coach.controller";
 
 const router = Router();
 
 router.put("/editProfile/:id", editProfile as any);
+router.put("/editBio/:id", editBio as any);
 
 router.get("/:id", getCoach as any);
 
