97e29806868455186b9ab491197ddc80b150d249
BreicheSinal <sinalbreiche@gmail.com>
2024-12-28 18:43:01 +0200

    Added a fedration service file

---
+++ src/modules/Federation/federation.service.ts
@@ -0,0 +1,81 @@
+import { AppDataSource } from "../../db/connection";
+import { User } from "../../db/entities/User";
+import { Federation } from "../../db/entities/Federation";
+import { Club } from "../../db/entities/Club";
+import { EditProfileInput, EditBioInput } from "../../schemas/generalSchema";
+
+const userRepository = AppDataSource.getRepository(User);
+const federationRepository = AppDataSource.getRepository(Federation);
+const clubRepository = AppDataSource.getRepository(Club);
+
+export const editProfileService = async (
+  id: number,
+  data: EditProfileInput
+) => {
+  const federation = await federationRepository.findOne({
+    where: { id },
+  });
+
+  if (!federation) {
+    throw new Error(`Federation with id ${id} not found`);
+  }
+
+  federation.location = data.location ?? null;
+  federation.country = data.country ?? null;
+  federation.founded_year = data.founded_year ?? null;
+
+  return federationRepository.save(federation);
+};
+
+export const editBioService = async (id: number, data: EditBioInput) => {
+  const federation = await federationRepository.findOne({
+    where: { id },
+    relations: ["user"],
+  });
+
+  if (!federation) {
+    throw new Error(`Federation with id ${id} not found`);
+  }
+
+  if (!federation.user) {
+    throw new Error(`User associated with federation of id ${id} not found`);
+  }
+
+  federation.user.bio = data.bio;
+  return userRepository.save(federation.user);
+};
+
+export const getClubsService = async (federationId: number) => {
+  const clubs = await clubRepository.find({
+    where: { federation: { id: federationId } },
+  });
+
+  if (clubs.length === 0) {
+    throw new Error(`Clubs with federation id ${federationId} not found`);
+  }
+
+  return clubs;
+};
+
+export const getFederationsService = async () => {
+  const federations = await federationRepository.find();
+
+  if (federations.length === 0) {
+    throw new Error("Federations not found");
+  }
+
+  return federations;
+};
+
+export const getFederationService = async (id: number) => {
+  const federation = await federationRepository.find({
+    where: { id },
+    relations: ["user"],
+  });
+
+  if (federation.length === 0) {
+    throw new Error(`Federation with id ${id} not found`);
+  }
+
+  return federation;
+};
