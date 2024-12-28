cbe46900098c70640f03b43b7aba87d11f486800
BreicheSinal <sinalbreiche@gmail.com>
2024-12-28 22:13:44 +0200

    Setup route get all connections

--- src/modules/User/user.routes.ts
+++ src/modules/User/user.routes.ts
@@ -1,10 +1,16 @@
 import { Router } from "express";
-import { createConnection, updateConnectionStatus } from "./user.controller";
+import {
+  createConnection,
+  updateConnectionStatus,
+  getConnections,
+} from "./user.controller";
 
 const router = Router();
 
 router.post("/:connectedUserId", createConnection as any);
 
 router.put("/:connectedUserId", updateConnectionStatus as any);
+
+router.get("/", getConnections as any);
 
 export default router;
