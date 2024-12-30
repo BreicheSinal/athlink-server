b3b41c5ff30e64e39c01db55932c22ed276aacbd
BreicheSinal <sinalbreiche@gmail.com>
2024-12-30 16:43:58 +0200

    Added routes for chats

--- src/modules/User/user.routes.ts
+++ src/modules/User/user.routes.ts
@@ -6,11 +6,17 @@ import {
   getPendingConnections,
   searchUsers,
   getStatusConnection,
+  createChat,
+  getUserChats,
+  sendMessage,
+  getChatMessages,
 } from "./user.controller";
 
 const router = Router();
 
 router.post("/:connectedUserId", createConnection as any);
+router.post("/chats", createChat as any);
+router.post("/chats/messages", sendMessage as any);
 
 router.put("/:connectedUserId", updateConnectionStatus as any);
 
@@ -18,5 +24,7 @@ router.get("/", getConnections as any);
 router.get("/pending", getPendingConnections as any);
 router.get("/:currentUserId/search", searchUsers as any);
 router.get("/:connectedUserId", getStatusConnection as any);
+router.get("/chats/user/:userId", getUserChats as any);
+router.get("/chats/:chatId/messages", getChatMessages as any);
 
 export default router;
