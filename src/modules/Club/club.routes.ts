a7c77c2ffc02eba577237e136447bde13ce782d2
BreicheSinal <sinalbreiche@gmail.com>
2025-01-06 21:28:35 +0200

    Added a route for delete try out method

--- src/modules/Club/club.routes.ts
+++ src/modules/Club/club.routes.ts
@@ -6,7 +6,8 @@ import {
   getClub,
   getClubUserID,
   getClubs,
-  addTryout,
+  addTryOut,
+  deleteTryOut,
 } from "./club.controller";
 
 const router = Router();
@@ -14,11 +15,13 @@ const router = Router();
 router.put("/editProfile/:id", editProfile as any);
 router.put("/editBio/:id", editBio as any);
 
-router.post("/tryouts", addTryout as any);
+router.post("/tryouts", addTryOut as any);
 
 router.get("/getStaff/:id", getStaff as any);
 router.get("/:id", getClub as any);
 router.get("/user/:id", getClubUserID as any);
 router.get("/", getClubs as any);
+
+router.delete("/deleteTr/:trID", deleteTryOut as any);
 
 export default router;
