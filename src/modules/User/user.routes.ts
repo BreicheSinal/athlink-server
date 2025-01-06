9c74a979f6bc9ad06c8abf9db973bde10af95fe9
BreicheSinal <sinalbreiche@gmail.com>
2025-01-06 23:32:00 +0200

    Addded routes for user controller methods

--- src/modules/User/user.routes.ts
+++ src/modules/User/user.routes.ts
@@ -12,6 +12,8 @@ import {
   sendMessage,
   getChatMessages,
   addExperienceCertification,
+  editExperienceCertification,
+  deleteExperienceCertification,
 } from "./user.controller";
 
 const router = Router();
@@ -22,6 +24,7 @@ router.post("/chats/messages", sendMessage as any);
 router.post("/addExpCert/:user_id", addExperienceCertification as any);
 
 router.put("/:connectedUserId", updateConnectionStatus as any);
+router.put("/editExpCert/:exp_id", editExperienceCertification as any);
 
 router.get("/", getConnections as any);
 router.get("/pending", getPendingConnections as any);
@@ -30,5 +33,7 @@ router.get("/:currentUserId/search", searchUsers as any);
 router.get("/:connectedUserId", getStatusConnection as any);
 router.get("/chats/user/:userId", getUserChats as any);
 router.get("/chats/messages", getChatMessages as any);
+
+router.delete("/deleteExpCert/:exp_id", deleteExperienceCertification as any);
 
 export default router;
