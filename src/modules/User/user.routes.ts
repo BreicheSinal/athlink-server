8224aec3de65da3ccc52fbd53fe593e540bfb601
BreicheSinal <sinalbreiche@gmail.com>
2025-01-09 16:50:09 +0200

    Updated route of pending connections --user-routes

--- src/modules/User/user.routes.ts
+++ src/modules/User/user.routes.ts
@@ -27,7 +27,7 @@ router.put("/:connectedUserId", updateConnectionStatus as any);
 router.put("/editExpCert/:exp_id", editExperienceCertification as any);
 
 router.get("/", getConnections as any);
-router.get("/pending", getPendingConnections as any);
+router.get("/pending/:userId", getPendingConnections as any);
 router.get("/accepted/:userId", getAcceptedConnections as any);
 router.get("/:currentUserId/search", searchUsers as any);
 router.get("/:connectedUserId", getStatusConnection as any);
