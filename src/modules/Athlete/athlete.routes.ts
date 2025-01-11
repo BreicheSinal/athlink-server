9ed5ac39d2aa375ddc98e0bef90eb36b488e2de4
BreicheSinal <sinalbreiche@gmail.com>
2025-01-11 15:59:34 +0200

    Added route for apply try out method --athlete-routes

--- src/modules/Athlete/athlete.routes.ts
+++ src/modules/Athlete/athlete.routes.ts
@@ -5,6 +5,7 @@ import {
   addTrophy,
   getAthlete,
   getAthleteUserID,
+  applyTryOut,
 } from "./athlete.controller";
 
 const router = Router();
@@ -13,10 +14,9 @@ router.put("/editProfile/:id", editProfile as any);
 router.put("/editBio/:id", editBio as any);
 
 router.post("/addTrophy/:entity_id", addTrophy as any);
-
+router.post("/tryout/apply", applyTryOut as any);
 
 router.get("/:id", getAthlete as any);
 router.get("/user/:id", getAthleteUserID as any);
-
 
 export default router;
