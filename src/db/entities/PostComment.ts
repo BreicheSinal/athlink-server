f5045790bec86b339bbcb31e92c3c3a8c391bbab
BreicheSinal <sinalbreiche@gmail.com>
2025-01-14 18:09:51 +0200

    Updated post comment entity

--- src/db/entities/PostComment.ts
+++ src/db/entities/PostComment.ts
@@ -12,29 +12,27 @@ import { Post } from "./Post";
 import { User } from "./User";
 
 @Entity("post_comment")
-@Index(["post"])
-@Index(["user"])
 export class PostComment {
   @PrimaryGeneratedColumn("increment", {
     type: "bigint",
     unsigned: true,
   })
-  id!: number;
+  id: number;
 
   @ManyToOne(() => Post, (post) => post.id, { onDelete: "CASCADE" })
   @JoinColumn({ name: "post_id" })
-  post!: Post;
+  post: Post;
 
   @ManyToOne(() => User, (user) => user.id, { onDelete: "CASCADE" })
   @JoinColumn({ name: "user_id" })
-  user!: User;
+  user: User;
 
   @Column({
     type: "text",
     nullable: false,
   })
-  comment!: string;
+  comment: string;
 
   @CreateDateColumn({ type: "datetime" })
-  createdAt!: Date;
+  created_at: Date;
 }
