27ba83428c6d4e7fbc2b1d14bcd9fa850f6e0355
BreicheSinal <sinalbreiche@gmail.com>
2025-01-21 14:45:45 +0200

    Deleted unnecessary files

--- src/modules/Post/post.routes.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { createPost } from "./post.controller";
-import { multerMiddleware } from "../../middlewares/multer";
-
-const router = express.Router();
-
-router.post("/create/:userId", multerMiddleware, createPost as any);
-
-export default router;
